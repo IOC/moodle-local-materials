6ad3d35980ea1753a8d3fb6dad77be3f0e982256
mcagigas <sedras@gmail.com>
2013-11-29 12:50:36 +0100

    new version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2013100801;
+$plugin->version   = 2013112901;
 $plugin->release   = '1.0.0';
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->requires  = 2013051401;
